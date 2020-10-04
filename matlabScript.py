35897ae8b995c791dd4ccc340e48904c0ce45517
Girish Hari <bunnefant@gmail.com>
2020-10-03 23:59:42 -0400

    passed name to flask

--- matlabScript.py
+++ matlabScript.py
@@ -6,9 +6,9 @@ matlabEngine = matlab.engine.start_matlab()
 #
 # with open("myScript.m","w+") as f:
 #     f.write(code)
-
+path = "C:/Users/bunne/Desktop/hackathons/musicalNoteRecognition"
 # eng.addpath(r'C:\Users\bunne\Desktop\hackathons\musicalNoteRecognition\matlabCode',nargout=0)
 # matlabEngine.evalc('cd C:\Users\bunne\Desktop\hackathons\musicalNoteRecognition')
-# matlabEngine.cd(r'C:\Users\bunne\Desktop\hackathons\musicalNoteRecognition')
-print(matlabEngine.pyth(float(3),float(5)))
+matlabEngine.cd(path)
+print(matlabEngine.convertMP3("2020-2021 TMEA Trumpet Etude 1 D major - Con Fuoco.mp3"))
 matlabEngine.quit()
